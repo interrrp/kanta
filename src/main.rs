9b8263f82a944d1c2479fcf9741bd01da021a651
interrrp <interrrp@proton.me>
2026-02-13 14:43:36 +0800

    Play audio files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env::args, fs::File, io::BufReader, process::exit};
+
+use anyhow::Context;
+use rodio::{OutputStreamBuilder, play};
+
+fn main() -> anyhow::Result<()> {
+    let stream_handle = OutputStreamBuilder::open_default_stream()?;
+    let mixer = stream_handle.mixer();
+
+    let file = File::open(args().skip(1).next().unwrap_or_else(|| {
+        eprintln!("usage: kanta <audio file path>");
+        exit(1);
+    }))
+    .context("failed to open audio file")?;
+
+    let sink = play(mixer, BufReader::new(file))?;
+    sink.sleep_until_end();
+
+    Ok(())
 }
