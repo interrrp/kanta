7c07ba2fcb7138b1dab85362b645393ed7315809
interrrp <interrrp@proton.me>
2026-02-15 15:40:04 +0800

    Enlarge default window size

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() -> iced::Result {
     application(Kanta::new, Kanta::update, Kanta::view)
         .subscription(Kanta::subscription)
         .title("Kanta")
-        .window_size((640, 360))
+        .window_size((1280, 720))
         .settings(Settings {
             default_text_size: Pixels(14.0),
             ..Default::default()
